7c8f7af0b5c29b7b2d4e7ad69cd6029611948c2b
Anaximandr228 <alex09.g29@gmail.com>
2024-10-16 00:46:46 +0500

    Reformat code

--- app/models.py
+++ app/models.py
@@ -5,19 +5,6 @@ from sqlalchemy.orm import relationship
 Base = declarative_base()
 
 
-# Определение полей таблицы Product_type
-class ProductType(Base):
-    __tablename__ = "product_type"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    time_created = Column(DateTime(timezone=True), server_default=func.now())
-    time_updated = Column(DateTime(timezone=True), onupdate=func.now())
-    time_deleted = Column(DateTime(timezone=True))
-
-    product = relationship("Product", back_populates="type")
-
-
 # Определение полей таблицы Product
 class Product(Base):
     __tablename__ = "product"
@@ -30,3 +17,16 @@ class Product(Base):
     product_type_id = Column(Integer, ForeignKey("product_type.id"))
 
     type = relationship("ProductType", back_populates="product")
+
+
+# Определение полей таблицы Product_type
+class ProductType(Base):
+    __tablename__ = "product_type"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    time_created = Column(DateTime(timezone=True), server_default=func.now())
+    time_updated = Column(DateTime(timezone=True), onupdate=func.now())
+    time_deleted = Column(DateTime(timezone=True))
+
+    product = relationship("Product", back_populates="type")
--- app/shemas.py
+++ app/shemas.py
@@ -34,7 +34,6 @@ class Product(ProductBase):
     id: int
     time_created: datetime
     time_updated: Optional[datetime]
-    time_deleted: Optional[datetime]
     name: str
     product_type_id: int
     type: ProductType
